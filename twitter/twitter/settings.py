cbdcd4573705e39010995c8785fce3dc6012b178
Akshad <akshadtupe0@gmail.com>
2026-01-31 21:17:38 +0530

    Index working

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -121,6 +121,6 @@ STATIC_URL = "static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
