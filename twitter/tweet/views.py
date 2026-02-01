dcacb82c08204c40664fd98d46e211bacc75df15
Akshad <akshadtupe0@gmail.com>
2026-02-01 17:10:26 +0530

    create html files for all views

--- twitter/tweet/views.py
+++ twitter/tweet/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-from dgnago.shortcuts import get_object_or_404
-from djnago.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     return render(request, "tweets/index.html")
 
-def tweet_list(request, tweet_id):
+def tweet_list(request, tweet_id=None):
     tweets = Tweet.objects.all().order_by("-created_at")
-    tweet = get_object_or_404(Tweet, pk=tweet_id)
+    tweet = None
+    if tweet_id is not None:
+        tweet = get_object_or_404(Tweet, pk=tweet_id)
     return render(request, "tweets/tweet_list.html", {"tweets": tweets, "tweet": tweet})
 
 def tweet_create(request):
@@ -21,10 +23,10 @@ def tweet_create(request):
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
-            return redirect("tweet_list", tweet_id=tweet.id)
+            return redirect("tweet_form", tweet_id=tweet.id)
     else:
         form = TweetForm()
-    return render(request, "tweets/tweet_create.html", {"form": form})
+    return render(request, "tweets/tweet_form.html", {"form": form})
     
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id ,user = request.user)
@@ -34,10 +36,10 @@ def tweet_edit(request, tweet_id):
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
-            return redirect("tweet_list", tweet_id=tweet.id)
+            return redirect("tweet_form", tweet_id=tweet.id)
     else:
         form = TweetForm(instance=tweet)
-    return render(request, "tweets/tweet_edit.html", {"form": form, "tweet": tweet})
+    return render(request, "tweets/tweet_form.html", {"form": form, "tweet": tweet})
 
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
